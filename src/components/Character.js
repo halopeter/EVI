615706f59139428f82b6166a47378af9aed62e11
halopeter <flamesea02@gmail.com>
2019-02-26 17:36:41 +0100

    Character and Navbar

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
 
 import NavBar from './Navbar';
+import eve from './eve.jpg';
 
 class Character extends Component {
     render() {
         return(
             <div>
                 <NavBar />
+                <Image src={eve}/>
             </div>
         );
     };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
 
 class NavBar extends Component {
     render() {
         return(
-            <Navbar>
-            <Nav className="mr-auto">
-                <Button>Yeah</Button>
-            </Nav>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">EVE Virtual Interface</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="character">Character</Nav.Link>
+                    <Nav.Link href="fittings">Fittings</Nav.Link>
+                    <Nav.Link href="corporation">Corporation</Nav.Link>
+                    <Nav.Link href="assets">Assets</Nav.Link>
+                    <Nav.Link href="industry">Industry</Nav.Link>
+                    <Nav.Link href="status">Status</Nav.Link>
+                </Nav>
+                <Nav>
+                <Nav.Link href="logout">Logout</Nav.Link>                    
+                </Nav>
             </Navbar>
         );
     };
