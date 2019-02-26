807dd93b85fb6f63112095feeebfc6c9ac5b293b
halopeter <flamesea02@gmail.com>
2019-02-26 14:02:22 +0100

    Init

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/Login.css'
+
+class Login extends Component {
+    render() {
+        return(
+                <div>
+                    <header className="LoginHeader">Welcome to EVE Virtual Interface</header>
+                    <p1>One application to rule them all</p1>
+                </div>
+        );
+    };
+}
+
+export default Login;
